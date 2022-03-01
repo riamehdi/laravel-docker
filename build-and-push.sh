a8bacc37c06adc4bb1d8165d2fd78d8331e51679
KHADIR <m@khadir.net>
2022-03-01 22:58:11 +0100

    update ci

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,5 +5,3 @@ docker build --compress --cache-from riamehdi/laravel-docker:7.4 --build-arg BUI
 docker run -t --rm -v "$(pwd)":/var/www/html riamehdi/laravel-docker:7.4 goss -g tests/goss-7.4.yaml v
 docker tag riamehdi/laravel-docker:7.4 riamehdi/laravel-docker:7.4
 docker push riamehdi/laravel-docker:7.4
-
-docker build --compress --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t riamehdi/laravel-docker:7.4 -f 7.4/Dockerfile .
